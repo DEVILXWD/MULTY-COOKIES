9e4224a6bb4b78bfc5fed90472f915aba626cd11
D3V1L BR4ND XWD <159911628+aloneboydevil580@users.noreply.github.com>
2024-06-02 12:03:20 +0530

    Update single_enc.py

--- single_enc.py
+++ single_enc.py
@@ -1,6 +1,115 @@
-#ENCODED BY : DEVIL XD HERE
-#ENCRYPTION : Py3 MARSHAL+ZLIB+B64
-#GITHUB : https://github.com/MR-DEVIL-420
-#----------------------------------------------
-import marshal, base64, zlib
-exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJzVN1tsG9eVd2Y4FKk3KVoaiiI1siJLiiRSkm3ZkRzZtChLsmw5juQkoisII861RIkPZUjGNmMVQtEPZmFgacCLVVsHZYEgcLb98AL9yMcCmy5aYJH+zBgDmB3AgNHdn/7RTYAG+eq5l6Qk0kqQAvuzM8Nz75zXPXPOuedc/g86dNWUxq8+5xH6VyQjmYmgYHFkggwd2SBLRy7IwchGTFE+yDNI5n6KgmbZBLBG5gFaZDNAq1wDsFa2BOtYhOtx3acMQr9mygsyiGBlK0Bus6GMlWsruT5l4Y09kJHrgo1WImn9FN5/vW9+sAk34cYtyqnUU80N1evFivjGavz9PiuiOusrdVILa6rWacbNAZRlV/qCNqDWyk3YJjfLtkesbMe2MPOblk854Of2+e3YvssxIBFrAn52s2X/Sx3w7sD27/ve3DF0xCUfq5QJtoKmNvgJcmuVtko+J/BYqr6nfV+27XtlXZRHqMJ2ANaNO2Rnpd2nUdCDPbnOo6yvWl+UGXJXxzlW141GUMJ0m73DLaPbDIOWAQvxb/8RcAW7gsePilcpjtxmdxmHXdXRxiLucu7L5F77ATb24PZfINmFe36BPuaCJ/AJ3Cp3PGKDvbAW2uwr88lu7AQ+z8cc7i1lQ2dlNuT6f8Bqr8sizYzXKy3/ACk3af52YWe1tyj++D+2k6hMNz5xJP41bDsS3/OKN7vkY2mYAXUAi5XUtJlEKibh48ADdhVjJp/IDR7lBXz8+/KvtH7vd8g6f4Bs33fInvh+2SxzfzDm60aYfw0pvcEhqqsfDx3hn0N5J79erWcZKmdw6OYQ6Fsqzm4zt1ExuwG3EuuENbywRltpjYFX1yjrkJlF9IoO9n6of/AvhHGhnzGa38bvp3AiOX0nhLeT4XgsdPijudLvKy8i1T7JHJA29+ewK6u+QUaLqJ9dMPhQBEtKP2uw8YRhTtxNJHH0M/QVYfrWvrq6OrewcG1qemFp6OK1ZXg16in/aiKkYBxT6oDNAr+EAGAXPTeZfzqXWddMrbqpVS0/ihWIFTabyzb/B7V5He1U2J3kDubVlle+7TBJ/oD3XhVvAK0M7bAV2th7rMyRLIfdbfqYq9LG3UO5Q9yHNHOvdBrvPeJBfiF93HXzjdHofDgZC4uheHwrjBOiLEXgbUMKx86L4+Niup7yTFGq+BeiJt0k4lgSK+KWpOBwbFzsNxlcOJY0+HBsOwWDIsXWsWGWtrdxTAZiTUm3URdLRVfLL0zYMJfmJtAqwqXUwsSoS+BkmUtpBUw9CdMWDVO+sXl35rn92L9gtWNKaw3orQHNPq3bp3fnnzfYsxNaQ6fe0FlALF8EGea5rVVte/Ox7fHIZy0wKT6abVK3TWbMeUvTPzf+U2P2A83i0S0elT75OvvuwquR389WEVVna3WkaX6mW6nnZqWiq8QFSYoSj37GKE3Ao5Dko6sUv9q+Tr86GqW+XY1JUayQDkGab8JGP/55XbNqG9TqhvS6IdU09H9goo2a+JaUSNyOKzKxThGOsK2e2LZd4lJcgHJWWdWr1fXpdX2qqa9o1eFU5MtWZalV1u/Y6PdgE9xjf8NUNqvN/cIDKc4tom6UNB9IQ6Fy7ZiS1kPfyd4zHVGw0I7pxyZSOIqzg4LVzy18xho1G1iSsZJQ7CDwbRd4ZSR6s2tFLJUvMQp5Lq5LkWRYlKS74fF+3rAoRVrC4MA7himJ7yQVYrbBbyuwG4CFSykRxUO8SNQSicR2PJbABoMTxCniQc7XR6UtvFpSqfQCyk08vEY9XGB53vG8xfXwXI57/O7n62rLJa3lkk6e+Ywl39SWafrmeZ2LJLzjADy3NKiNp55ImuWsbjmrWs7mm1oyyfL9DVwFHthgTDhgmZ+4RZSr9fPcvzX7Efc7xMM0PVYr/pDLn0puxBWSPoHpd+au1KabaV4tJiUlKS6Fo3g8betZHuqJDvXIYs/seM/V8Z7FtDWAPwhHxDNn31hKC7VU4kp8HRy9mAqFcCJxKxXpqk331dJozIDzxXKidomB1JqkSGJSuVuqQt7a9PBGMrmdGPf51lKJcAwUeG9JIbwGhcQL+2ofuxqJhyTSixLp/7oaT4cjEcl32jss9l0Jx1J3JkR/TFbiYVkcGZkQ37763ujIqVPixVQ4Ivvenr8+4h0dHhkdOeMdHh6dEG9/0C/6t7cj+F28BlXUd/rkGe/JMbFvfnbp6pVBMRLewuIMDm3F+8V3ILtgTd8pWGlqQ4lHsW9k+KR32Ht6eGzYe2ZEvBpfC0ewuCjdkpRwWdPNSxdX5/wXfZcunvJPXLrof8d38sxp7wiIjZ71joCFK9D+zMXinK69kcDKkH8dCohCjoMKSaG0tW/a75/50e2BfoWcykqheSsOWT1Hd3y6jWICOCLdFfsWcSgORZsSRErwS9tQvaaKlUm8RIx8S0puEA6DgfarkJ1GQZqFXgCbKQpelqAD1Es0jqvJ+BaOpV3l8Kwr0vZGZWzStb5S6Uv4QINJliB1yKZJ871huXc83VRldalyBaC4iklIMIJqL3WqoqFrG3hTgj17VyI05+EuBp8TJocFKQKk1H/CMkA9E/3zw93/p0/aVaxYH+6siLDpkqmEOC76E5BKkaSU9hWJvhVI8C0glOMQ9a5JiXCoMhK+D3do46ICJV8SbR/u1Crkn9R6sai2Xkh37lfJ+TiUxdimJFXWSJ9C2pBCyrXBKhhKJATKaCnXTS8uHwoTtG7SExnt97Tv0TZDK6FRJ9H6shqO3Yor3bSUQpG8RdVZym3JsKYg/WmXopmjkMap9BBucwLOfaENgwMpg19X4qlto5Ym5SqWpHWlkXCSnmzUhOXVbUgyo7GyExu8TLaHYSuhV2/BNoC1khuGKQ6nHMNEEIZVgTYSIVXGsJTz2WDuGMxdg09EMN6GIamEt0m32EocaLsdTm4UOz75c2CYqAmmzUQ8RlQWm8aowUVgITO4ICollROE0bp/rq7q2N9azkXjciqCJ5UrtAVCL/lfOGUVOIZh8qhpl955ZN2l96FJ3S6982hYrXwKZoZpK6B9YDEzLQW0D+wM4yWEErAwTD+ZlQAwOwhfCdjZJtNuXcGCGPMuv7ulIbuO7CqyF8wUo5pdeyt7S49W1IFJzX1ed5/X0AUdXVDRBeCob9u1FFgzcz7f3JGtf9D4sHF3Ll/TnJ19WtOh1nTkLQ0ZXm0c0ixe3eJVLV5AvCji3tQsk7plUrVMFhpAvtA8zjQW2Df4RirzUc39mkxN3u7ODjwYejiUqYG12jwFNGx1vyQgcynf1p29p772Tr5dLLCoM8QVeE4CFYhCM3J25U6obf2FGk4YeIkAfE1AZrZQi5oFtakz33zsAf/CeXzP/siZm3jCwT377w2/bdCck7pz8pkz8NQZ+OK65pzRnTOZy/lWZza017PXo4qntfYxvX1Maz2jt57JzOTdPtXt09wjunsks5B3tGWn9vg9XvWMa8KELkxojnO641xmOi/25Hj1xLwmXtHFK2q9+4XtWNahtvY+btFsQ7ptCILTMPjEkXd1/nLy55Oaa0B3DWQtkCQNg5REwUsCvkYVuKMAOVAcRTMjh/BRIOPPvJ+vt2WmP5q5P5OZKZhcDe4C225z5x2ebODh5b2U5ujRHT2qoyfv6s75H008cw0+dQ1qLq/u8mYD+YE31L4peD5fKo7A+KL7VL5vFILQ0Qox6GjNBiClurw5yycNqnf1v2cAlJ5bm2pqp4DQBfYSC0OKmSFD1yz7ksKvKfwbwKvsXykk+GsUf43NzuXbxNxUbuqx9VcLnyyobaPw5AU38fjokxlNOKcL51ThXAl16kn0ifzbqBp4Vxt7Tx97TxOWdWFZFZZL9DfV829pwnVduK4K10u4c5/f0oRpXZhWhWmKAus6NziwwhnmvqawQGHe7cnZfnYjF8qFHvf9KvpJVOs4ucfsMS8E5971n5n3mJLCyWee6aee6S8kzTOre2Y1YU4X5lRhrkR++5ln+alnWQ0CfU33rGlCSBdCqhAq0cefefxPPX714qLmWdI9S5pwQxduqMKNPwlioQE5278pNCOhD7xucx+AvMOZnVbbL3zRpTkCuiOgOgLl6CYeXHt4LXst7+jIzpL7wSw9gNaAFIwvRyAb4HUc1R8jZ9fGA1DazqNP/JplTLeMqZYxiirf5XNsI1HzPgM7OrEB1e534xd7L0+g33cdm2b534/Vw/wPrGnabP2DmSNzK0PmtS1k7gn0XbayX1LCl1b+8mn+y8aOy8P8l6+bCWaYYk7zZD7Bz7dxf0T183buj3Yy/zsZWs4f'))))
+import requests
+import os
+import re
+import time
+from requests.exceptions import RequestException
+
+# Terminal screen ko clear karne ka function
+def clear_screen():
+    os.system("clear")
+
+# Cookies set up karne ka function
+def set_cookies():
+    cookies = []
+    num_cookies = int(input("\033[92mKitni cookies dalni hain? :: "))
+    for i in range(num_cookies):
+        cookie = input(f"\033[92mCookie {i+1} enter karein: ")
+        cookies.append(cookie)
+    return cookies
+
+# Commenter ka naam lekar aane ka function
+def get_commenter_name():
+    return input("\033[92mHater ka Naam :: ")
+
+# Password enter karne ka function
+def get_password():
+    return input("\033[92mPassword :: ")
+
+# Network requests handle karne ka function
+def make_request(url, headers, cookies):
+    try:
+        response = requests.get(url, headers=headers, cookies=cookies).text
+        return response
+    except RequestException as e:
+        print("\033[91m[!] Request mein galti aayi:", e)
+        return None
+
+# Author details
+author_info = """
+                                     Author :: DEVIL
+"""
+
+print(author_info)
+
+# Start time
+print("\033[92mStart Time:", time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# Login System
+password = "Devil 789"
+while True:
+    user_pass = get_password()
+    if user_pass == password:
+        print("\n\033[92mLogin Successful!\n")
+        break
+    else:
+        print("\n\033[91mGalt Password! Dubara try karein.\n")
+
+while True:
+    try:
+        print()
+        cookies = set_cookies()
+
+        # Loop through each cookie and make requests
+        for cookie in cookies:
+            response = make_request('https://business.facebook.com/business_locations', headers={
+                'Cookie': cookie,
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 11; RMX2144 Build/RKQ1.201217.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.71 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/375.1.0.28.111;]'
+            }, cookies={'Cookie': cookie})
+
+            if response is None:
+                break
+
+            token_eaag = re.search('(EAAG\w+)', str(response)).group(1)
+            id_post = int(input("\033[92mPost Id :: "))
+            commenter_name = get_commenter_name()
+            delay = int(input("\033[92mDelay (Second) :: "))
+            comment_file_path = input("\033[92mAapka Comment File Path :: ")
+
+            with open(comment_file_path, 'r') as file:
+                comments = file.readlines()
+
+            x, y = 0, 0
+            print()
+
+            while True:
+                try:
+                    time.sleep(delay)
+                    teks = comments[x].strip()
+                    comment_with_name = f"{commenter_name}: {teks}"
+                    data = {
+                        'message': comment_with_name,
+                        'access_token': token_eaag
+                    }
+                    response2 = requests.post(f'https://graph.facebook.com/{id_post}/comments/', data=data, cookies={'Cookie': cookie}).json()
+                    if '\'id\':' in str(response2):
+                        print("\033[92mPost Id ::", id_post)
+                        print("\033[92mDate time ::", time.strftime("%Y-%m-%d %H:%M:%S"))
+                        print("\033[92mComment bheja gaya ::", comment_with_name)
+                        print("\033[92mCookie ka istemal ::", cookie)
+                        print('\033[97m' + '──────────────────────────────────────────────────────────────')
+                        x = (x + 1) % len(comments)
+                    else:
+                        y += 1
+                        print("\033[91m[{}] Status : Asafalta".format(y))
+                        print("\033[91m[/]Link : https://m.basic.facebook.com//{}".format(id_post))
+                        print("\033[91m[/]Comments : {}\n".format(comment_with_name))
+                        continue
+
+                except RequestException as e:
+                    print("\033[91m[!] Request mein galti aayi:", e)
+                    time.sleep(5.5)
+                    continue
+
+    except Exception as e:
+        print("\033[91m[!] Koi anjaan galti aayi:", e)
+        break
